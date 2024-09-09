89c6cd58ae7f71a340875233b6169e004b01ebc9
Prince9g <142648202+Prince9g@users.noreply.github.com>
2024-09-09 17:11:48 +0530

    handling conflicts

--- src/App.jsx
+++ src/App.jsx
@@ -10,9 +10,11 @@ import ThirdPage from "./components/ThirdPage";
 import SecondPage from "./components/SecondPage";
 import ResumeBuilder from "./components/ResumeBuilder";
 import SecPart2 from "./components/SecPart2";
+import ChatBot from "./components/ChatBot";
 function App() {
   return (<>
     <NavBar/>
+    <ChatBot/>
     <Main/>
     <RecurCards/>
     <SecondPage/>
